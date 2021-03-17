c5b4142013c389ef8fac0892c4a9a15123f6b12f
Udayveer1 <udayveersmile1@gmail.com>
2021-03-17 15:47:45 +0530

    add constarined

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var chain,log6;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,8 +33,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
     bird = new Bird(100,100);
+ log6=new Log(230,180,80,PI/2)
+   chain= new Chain(bird.body,log6.body)
 
 }
 
@@ -58,7 +59,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+log6.display();
     bird.display();
     platform.display();
+ chain.display()
 }
